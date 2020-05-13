5c3df15475b028989e0894600fe847ee88b6df02
Jinnong Zhao <zjinnong@gmail.com>
2020-05-13 16:31:34 -0400

    create user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_11_072252) do
+ActiveRecord::Schema.define(version: 2020_05_13_202547) do
 
   create_table "carpools", force: :cascade do |t|
     t.boolean "doorToDoor"
@@ -24,6 +24,13 @@ ActiveRecord::Schema.define(version: 2020_05_11_072252) do
     t.datetime "time"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
